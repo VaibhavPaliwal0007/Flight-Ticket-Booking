23e0e5ab9b8b11a497d837fcfd3c742276877a3d
VaibhavPaliwal0007 <78612380+VaibhavPaliwal0007@users.noreply.github.com>
2023-06-30 05:00:54 +0530

    changesfg

--- frontend/components/MyBooking.js
+++ frontend/components/MyBooking.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+// import React from 'react';
+// import { useSelector } from 'react-redux';
 
-const MyBooking = async () => {
-  const token = useSelector(state => state.token);
-  const [bookings, setBookings] = useState([]);
+// const MyBooking = async () => {
+//   const token = useSelector(state => state.token);
+//   const [bookings, setBookings] = useState([]);
 
-  try {
-    const response = await fetch(`/bookings`, {
-      method: "GET",
-      headers: {
-        'Authorization': `Bearer ${token}`,
-      }
-    })
-    const data = await response.json();
-    setBookings(data.bookings);
-  }
-  catch (err) {
-    alert(err.message);
-  }
+//   try {
+//     const response = await fetch(`/bookings`, {
+//       method: "GET",
+//       headers: {
+//         'Authorization': `Bearer ${token}`,
+//       }
+//     })
+//     const data = await response.json();
+//     setBookings(data.bookings);
+//   }
+//   catch (err) {
+//     alert(err.message);
+//   }
 
 
-  return (
-    <div>
-      {/* {bookings.map(booking => (
-        <Card flight={booking} />))
-      } */} 
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       {/* {bookings.map(booking => (
+//         <Card flight={booking} />))
+//       } */} 
+//     </div>
+//   )
+// }
 
-export default MyBooking
+// export default MyBooking
