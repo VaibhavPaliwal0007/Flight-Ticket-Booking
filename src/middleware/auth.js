4e3c34a56bc972ccfbace756e156390d5bfad106
VaibhavPaliwal0007 <78612380+VaibhavPaliwal0007@users.noreply.github.com>
2023-06-29 17:10:13 +0530

    middleware done

---
+++ src/middleware/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken')
+
+const { User } = require('../models');
+
+const validateSession = async (req, res, next) => {
+    try {
+        const token = req.header("Authorization").replace("Bearer ", "");
+        const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await User.findOne({
+            _id: decoded._id,
+            "tokens.token": token,
+        });
+
+        if (!user) {
+            throw new Error();
+        }
+
+        req.token = token;
+        req.user = user;
+
+        next();
+    } catch (err) {
+        res.status(401).send({ error: "Please authenticate." });
+    }
+};
+
+module.exports = validateSession;
---
+++ src/models/index.js
@@ -0,0 +1,7 @@
+const User = require('./User');
+const FlightDetail = require('./FlightDetail');
+
+module.exports = {
+    User,
+    FlightDetail
+};
