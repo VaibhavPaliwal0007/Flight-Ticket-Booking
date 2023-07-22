caaf030c479334ccde5c78d5aee28aba18551e9d
Yashika Kaushik <yashikakaushik@Yashikas-MacBook-Air.local>
2023-07-23 00:09:47 +0530

    ui changes

--- frontend/components/home/Card.js
+++ frontend/components/home/Card.js
@@ -4,6 +4,11 @@ import { useSelector } from 'react-redux';
 export default function Card({ flight }) {
     const token = useSelector(state => state.auth.token);
     const router = useRouter();
+    let date = flight.date;
+    date = new Date(date);
+    date = date.toDateString();
+    // date = date.toString();
+    // let hours = date.getHours();
 
     const bookingHandler = async () => {
         try {
@@ -72,7 +77,7 @@ export default function Card({ flight }) {
                                     </p>
                                 </div>
                                 <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                    {flight.date}
+                                    {date}
                                 </div>
                             </div>
                         </li>
@@ -125,8 +130,8 @@ export default function Card({ flight }) {
                             </div>
                         </li>
                         <li className="flex justify-around pt-3 pb-0 sm:pt-4">
-                            <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={viewHandler}>View</button>
-                            <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={bookingHandler}>Book</button>
+                            <button type="button" class="focus:outline-none text-black bg-blue-200 hover:bg-blue-400 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2  " onClick={viewHandler}>View</button>
+                            <button type="button" class="focus:outline-none text-black bg-blue-200 hover:bg-blue-00 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 " onClick={bookingHandler}>Book</button>
                         </li>
                     </ul>
                 </div>
